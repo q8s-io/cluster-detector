c292055b7cb162b6428a8ff4ffa132dbc50a4819
李闯 <lichuang1@360.cn>
2020-12-30 16:31:00 +0800

    test

--- pkg/infrastructure/kubernetes/configs.go
+++ pkg/infrastructure/kubernetes/configs.go
@@ -3,6 +3,7 @@ package kubernetes
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/url"
 	"strconv"
 
@@ -121,14 +122,14 @@ func GetKubeClientConfig(uri *url.URL) (*restClient.Config, error) {
 }
 
 func GetKubernetesClient(uri *url.URL) (client kubernetes.Interface, err error) {
-	kubeConfig, err := GetKubeClientConfig(uri)
+	/*kubeConfig, err := GetKubeClientConfig(uri)
 	if err != nil {
 		return nil, err
-	}
-	/*kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
+	}*/
+	kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
 	if err != nil {
 		log.Println(err.Error())
-	}*/
+	}
 
 	kubeClient, err := kubernetes.NewForConfig(kubeConfig)
 	if err != nil {
